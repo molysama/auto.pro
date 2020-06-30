dd787f6ee55d8156cdecdb9cbe3e3c797e7b1e9b
molysama <molysama.i@gmail.com>
2020-06-30 21:51:43 +0800

    完善AES

--- packages/webpack-plugin/__tests__/index.js
+++ packages/webpack-plugin/__tests__/index.js
@@ -1,1 +1,11 @@
-console.log('hello,world')
+
+const CryptoJS = require("crypto-js")
+const fs = require('fs')
+const path = require('path')
+
+var decode = CryptoJS.AES.decrypt(fs.readFileSync(path.resolve(__dirname, 'dist/first.js'), 'utf8'), 'SecretPassphrase', {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7,
+}).toString(CryptoJS.enc.Utf8)
+
+fs.writeFileSync(path.resolve(__dirname, 'dist/decode.js'), decode)
--- packages/webpack-plugin/__tests__/webpack.config.js
+++ packages/webpack-plugin/__tests__/webpack.config.js
@@ -4,12 +4,12 @@ const AutoProWebpackPlugin = require('..')
 
 module.exports = {
     entry: {
-        index: path.resolve(__dirname, 'index.js'),
+        first: path.resolve(__dirname, 'first.js'),
         second: path.resolve(__dirname, 'second.js')
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
+        // filename: '[name].[chunkhash].js'
     },
     plugins: [
         new AutoProWebpackPlugin({
