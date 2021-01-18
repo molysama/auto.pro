a0e6411d16fc833e81cbb092f26e8c1a5f7c22b5
molysama <molysama.i@gmail.com>
2021-01-18 19:45:14 +0800

    修改类型提示

--- packages/web/src/index.d.ts
+++ packages/web/src/index.d.ts
@@ -1,5 +1,5 @@
 export declare const AutoWeb: {
-    setMode(mode: 'console' | 'prompt'): void;
+    setMode(mode: 'console.log' | 'prompt'): void;
     devicelly(deviceFn: string, ngFn: Function, self?: any, once?: boolean): void;
     removeDevicelly(deviceFn: string): void;
     auto(eventname: string, params?: any, callback?: Function): any;
