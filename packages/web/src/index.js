ff55ecbb978e54373f6c5662752d08fd16ffeda8
molysama <molysama.i@gmail.com>
2021-01-18 21:37:26 +0800

    将umd的es6转为es5

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,42 +1,61 @@
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-        typeof define === 'function' && define.amd ? define(factory) :
-            (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.AutoWeb = factory());
-}(this, (function () {
-    'use strict';
+    if (typeof define === "function" && define.amd) {
+        define(["exports"], factory);
+    } else if (typeof exports !== "undefined") {
+        factory(exports);
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod.exports);
+        global.repl = mod.exports;
+    }
+})(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : this, function (_exports) {
+    "use strict";
 
-    let emit = prompt;
-    var source = {
-        setMode(mode) {
+    Object.defineProperty(_exports, "__esModule", {
+        value: true
+    });
+    _exports.default = void 0;
+    var emit = prompt;
+    var _default = {
+        setMode: function setMode(mode) {
             emit = window[mode] || prompt;
         },
-        devicelly(deviceFn, ngFn, self, once = false) {
-            window[deviceFn] = (...option) => {
-                const result = ngFn.apply(self || this, option);
+        devicelly: function devicelly(deviceFn, ngFn, self) {
+            var _this = this;
+
+            var once = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
+
+            window[deviceFn] = function () {
+                for (var _len = arguments.length, option = new Array(_len), _key = 0; _key < _len; _key++) {
+                    option[_key] = arguments[_key];
+                }
+
+                var result = ngFn.apply(self || _this, option);
+
                 if (once) {
                     delete window[deviceFn];
                 }
+
                 return result;
             };
         },
-        removeDevicelly(deviceFn) {
+        removeDevicelly: function removeDevicelly(deviceFn) {
             delete window[deviceFn];
         },
-        auto(eventname, params, callback) {
+        auto: function auto(eventname, params, callback) {
             if (callback) {
-                const EVENT_ID = eventname + Date.now().toString();
+                var EVENT_ID = eventname + Date.now().toString();
                 this.devicelly(EVENT_ID, callback, this, true);
                 return emit(eventname, JSON.stringify({
-                    params,
+                    params: params,
                     PROMPT_CALLBACK: EVENT_ID
                 }));
-            }
-            else {
+            } else {
                 return emit(eventname, JSON.stringify(params));
             }
         }
     };
-
-    return source;
-
-})));
+    _exports.default = _default;
+});
